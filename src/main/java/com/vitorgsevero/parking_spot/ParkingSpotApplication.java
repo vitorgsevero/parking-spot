74c9323d83dc9ba8f76700cf1fc23d0259547cc4
Vítor Severo <vitor_severo@sicredi.com.br>
2024-07-02 16:09:00 -0300

    initial commit

---
+++ src/main/java/com/vitorgsevero/parking_spot/ParkingSpotApplication.java
@@ -0,0 +1,13 @@
+package com.vitorgsevero.parking_spot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingSpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitorgsevero/parking_spot/ParkingSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorgsevero.parking_spot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
