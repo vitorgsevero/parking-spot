8d209634caf138a0be34c9814a243e2dc3efddcd
Vítor Severo <vitorgsevero@gmail.com>
2024-07-03 19:13:49 -0300

    feat: adding parking spot repository, service and controller

---
+++ src/main/java/com/vitorgsevero/parkingspot/service/ParkingSpotService.java
@@ -0,0 +1,15 @@
+package com.vitorgsevero.parkingspot.service;
+
+import com.vitorgsevero.parkingspot.repository.ParkingSpotRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+}
